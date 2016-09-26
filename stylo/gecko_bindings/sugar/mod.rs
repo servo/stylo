a2e388d73ccc13a3f9f05c53186115429f50d445
Manish Goregaokar <manishsmail@gmail.com>
2016-09-26 02:36:05 -0500

    servo: Merge #13372 - Merge most of geckolib into style (from Manishearth:merge-stylo); r=emilio
    Fixes #13038
    
    r? @emilio
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: f763eca344fa6a49f2467d9baa25044bebc41ff2
    
    --HG--
    rename : servo/ports/geckolib/binding_tools/.gitignore => servo/components/style/binding_tools/.gitignore
    rename : servo/ports/geckolib/binding_tools/README.md => servo/components/style/binding_tools/README.md
    rename : servo/ports/geckolib/binding_tools/regen.py => servo/components/style/binding_tools/regen.py
    rename : servo/ports/geckolib/binding_tools/regen.sh => servo/components/style/binding_tools/regen.sh
    rename : servo/ports/geckolib/binding_tools/regen_atoms.py => servo/components/style/binding_tools/regen_atoms.py
    rename : servo/ports/geckolib/binding_tools/setup_bindgen.sh => servo/components/style/binding_tools/setup_bindgen.sh
    rename : servo/ports/geckolib/context.rs => servo/components/style/gecko/context.rs
    rename : servo/components/style/gecko_conversions.rs => servo/components/style/gecko/conversions.rs
    rename : servo/ports/geckolib/data.rs => servo/components/style/gecko/data.rs
    rename : servo/components/style/generated/gecko_pseudo_element_helper.rs => servo/components/style/gecko/generated/gecko_pseudo_element_helper.rs
    rename : servo/components/style/gecko_selector_impl.rs => servo/components/style/gecko/selector_impl.rs
    rename : servo/ports/geckolib/snapshot.rs => servo/components/style/gecko/snapshot.rs
    rename : servo/ports/geckolib/snapshot_helpers.rs => servo/components/style/gecko/snapshot_helpers.rs
    rename : servo/ports/geckolib/traversal.rs => servo/components/style/gecko/traversal.rs
    rename : servo/components/style/gecko_values.rs => servo/components/style/gecko/values.rs
    rename : servo/ports/geckolib/wrapper.rs => servo/components/style/gecko/wrapper.rs
    rename : servo/ports/geckolib/gecko_bindings/bindings.rs => servo/components/style/gecko_bindings/bindings.rs
    rename : servo/ports/geckolib/gecko_bindings/lib.rs => servo/components/style/gecko_bindings/mod.rs
    rename : servo/ports/geckolib/gecko_bindings/ptr.rs => servo/components/style/gecko_bindings/ptr.rs
    rename : servo/ports/geckolib/gecko_bindings/structs_debug.rs => servo/components/style/gecko_bindings/structs_debug.rs
    rename : servo/ports/geckolib/gecko_bindings/structs_release.rs => servo/components/style/gecko_bindings/structs_release.rs
    rename : servo/ports/geckolib/gecko_bindings/sugar/mod.rs => servo/components/style/gecko_bindings/sugar/mod.rs
    rename : servo/ports/geckolib/gecko_bindings/sugar/ns_css_shadow_array.rs => servo/components/style/gecko_bindings/sugar/ns_css_shadow_array.rs
    rename : servo/ports/geckolib/gecko_bindings/sugar/ns_style_auto_array.rs => servo/components/style/gecko_bindings/sugar/ns_style_auto_array.rs
    rename : servo/ports/geckolib/gecko_bindings/sugar/ns_style_coord.rs => servo/components/style/gecko_bindings/sugar/ns_style_coord.rs
    rename : servo/ports/geckolib/gecko_bindings/sugar/ns_t_array.rs => servo/components/style/gecko_bindings/sugar/ns_t_array.rs
    rename : servo/ports/geckolib/gecko_bindings/sugar/ownership.rs => servo/components/style/gecko_bindings/sugar/ownership.rs
    rename : servo/ports/geckolib/string_cache/lib.rs => servo/components/style/gecko_string_cache/mod.rs
    rename : servo/ports/geckolib/string_cache/namespace.rs => servo/components/style/gecko_string_cache/namespace.rs

---
+++ stylo/gecko_bindings/sugar/mod.rs
@@ -0,0 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+mod ns_css_shadow_array;
+mod ns_style_auto_array;
+pub mod ns_style_coord;
+mod ns_t_array;
+pub mod ownership;

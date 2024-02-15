610b0216f9923493f2564349d2f58ea0eee57998
Delan Azabani <dazabani@igalia.com>
2024-02-15 18:30:14 +0800

    Commit our changes on top of upstream Stylo
    This is a rebase of 40aa59d7f3b2bce517c480c95a80d639843e1449
    
    Signed-off-by: Oriol Brufau <obrufau@igalia.com>

--- sync.sh
+++ sync.sh
@@ -30,7 +30,7 @@ fi
 
 step Cloning upstream if needed
 if ! [ -e upstream ]; then
-    git clone --bare --single-branch --progress https://github.com/mozilla/gecko-dev.git upstream
+    git clone --bare --single-branch --branch main --progress https://github.com/mozilla-firefox/firefox.git upstream
 fi
 
 step Updating upstream

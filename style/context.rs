7f4da613eec9dd8210d32b0f55aea1143024ace4
Martin Robinson <mrobinson@igalia.com>
2024-02-23 09:14:10 +0100

    script: Do not run layout in a thread (servo#31346)
    * script: Do not run layout in a thread
    
    Instead of spawning a thread for layout that almost always runs
    synchronously with script, simply run layout in the script thread.
    
    This is a resurrection of #28708, taking just the bits that remove the
    layout thread. It's a complex change and thus is just a first step
    toward cleaning up the interface between script and layout. Messages are
    still passed from script to layout via a `process()` method and script
    proxies some messages to layout from other threads as well.
    
    Big changes:
    
    1. Layout is created in the script thread on Document load, thus every
       live document is guaranteed to have a layout. This isn't completely
       hidden in the interface, but we can safely `unwrap()` on a Document's
       layout.
    2. Layout configuration is abstracted away into a LayoutConfig struct
       and the LayoutFactory is a struct passed around by the Constellation.
       This is to avoid having to monomorphize the entire script thread
       for each layout.
    3. Instead of having the Constellation block on the layout thread to
       figure out the current epoch and whether there are pending web fonts
       loading, updates are sent synchronously to the Constellation when
       rendering to a screenshot. This practically only used by the WPT.
    
    A couple tests start to fail, which is probably inevitable since removing
    the layout thread has introduced timing changes in "exit after load" and
    screenshot behavior.
    
    Co-authored-by: Josh Matthews <josh@joshmatthews.net>
    
    * Update test expectations
    
    * Fix some issues found during review
    
    * Clarify some comments
    
    * Address review comments
    
    ---------
    
    Co-authored-by: Josh Matthews <josh@joshmatthews.net>

--- style/context.rs
+++ style/context.rs
@@ -478,7 +478,7 @@ impl<E: TElement> SequentialTask<E> {
     /// Executes this task.
     pub fn execute(self) {
         use self::SequentialTask::*;
-        debug_assert_eq!(thread_state::get(), ThreadState::LAYOUT);
+        debug_assert!(thread_state::get().contains(ThreadState::LAYOUT));
         match self {
             Unused(_) => unreachable!(),
             #[cfg(feature = "gecko")]
@@ -554,7 +554,7 @@ where
     E: TElement,
 {
     fn drop(&mut self) {
-        debug_assert_eq!(thread_state::get(), ThreadState::LAYOUT);
+        debug_assert!(thread_state::get().contains(ThreadState::LAYOUT));
         for task in self.0.drain(..) {
             task.execute()
         }

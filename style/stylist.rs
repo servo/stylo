e395ae53fe77c8d94731b343fda7a1a6c07352a3
Oriol Brufau <obrufau@igalia.com>
2024-02-28 00:11:40 +0100

    Fixup for https://phabricator.services.mozilla.com/D182110

--- style/stylist.rs
+++ style/stylist.rs
@@ -1699,7 +1699,6 @@ impl<T: 'static> LayerOrderedMap<T> {
     fn clear(&mut self) {
         self.0.clear();
     }
-    #[cfg(feature = "gecko")]
     fn try_insert(&mut self, name: Atom, v: T, id: LayerId) -> Result<(), AllocErr> {
         self.try_insert_with(name, v, id, |_, _| Ordering::Equal)
     }
@@ -1723,7 +1722,6 @@ impl<T: 'static> LayerOrderedMap<T> {
         vec.push((v, id));
         Ok(())
     }
-    #[cfg(feature = "gecko")]
     fn sort(&mut self, layers: &[CascadeLayer]) {
         self.sort_with(layers, |_, _| Ordering::Equal)
     }

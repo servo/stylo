3be814ceef52f32b1b853305420a3a79678836a7
Oriol Brufau <obrufau@igalia.com>
2024-02-28 01:44:55 +0100

    Fixup for https://phabricator.services.mozilla.com/D172395

--- style/values/specified/text.rs
+++ style/values/specified/text.rs
@@ -286,6 +286,7 @@ impl Parse for TextTransform {
                 "capitalize" if result.case_ == TextTransformCase::None => {
                     result.case_ = TextTransformCase::Capitalize
                 },
+                #[cfg(feature = "gecko")]
                 "math-auto" if result.case_ == TextTransformCase::None &&
                     result.other_.is_empty() => {
                     result.case_ = TextTransformCase::MathAuto;
@@ -356,6 +357,7 @@ pub enum TextTransformCase {
     /// Capitalize each word.
     Capitalize,
     /// Automatic italicization of math variables.
+    #[cfg(feature = "gecko")]
     MathAuto,
 }
 

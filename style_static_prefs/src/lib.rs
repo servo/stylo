fe08f13bedf17cc75e4a348d214b66241ecc6321
Oriol Brufau <obrufau@igalia.com>
2024-03-01 10:01:20 +0100

    Fixup for https://phabricator.services.mozilla.com/D183528

--- style_static_prefs/src/lib.rs
+++ style_static_prefs/src/lib.rs
@@ -24,6 +24,9 @@ macro_rules! pref {
     ("layout.css.abs-sign.enabled") => {
         false
     };
+    ("layout.css.basic-shape-rect.enabled") => {
+        false
+    };
     ("layout.css.basic-shape-xywh.enabled") => {
         false
     };

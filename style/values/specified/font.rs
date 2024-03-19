ecc54a98bddd2fd780bb4b39d41641c55d7c040b
Munish Mummadi <141582088+MunishMummadi@users.noreply.github.com>
2024-03-19 11:58:37 -0500

    Enable x-lang attribute for Servo engine (#24)
    * Enable x-lang attribute
    
    * updated-serialize-deserialize

--- style/values/specified/font.rs
+++ style/values/specified/font.rs
@@ -2029,6 +2029,7 @@ impl XTextScale {
     ToResolvedValue,
     ToShmem,
 )]
+#[cfg_attr(feature = "servo", derive(Deserialize, Serialize))]
 /// Internal property that reflects the lang attribute
 pub struct XLang(#[css(skip)] pub Atom);
 

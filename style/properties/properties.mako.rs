2ec85f99d38eb7e780cf88e7efa101fcb495ed1b
Oriol Brufau <obrufau@igalia.com>
2024-04-09 18:10:45 +0200

    Fixup for https://phabricator.services.mozilla.com/D193319

--- style/properties/properties.mako.rs
+++ style/properties/properties.mako.rs
@@ -1384,7 +1384,6 @@ pub mod style_structs {
     use fxhash::FxHasher;
     use super::longhands;
     use std::hash::{Hash, Hasher};
-    use crate::logical_geometry::WritingMode;
     use crate::media_queries::Device;
     use crate::values::computed::NonNegativeLength;
 

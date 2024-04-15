e2690e0ed644a2bf41e9a6e510373667c6052fb6
Oriol Brufau <obrufau@igalia.com>
2024-04-15 14:40:54 +0200

    Fixup for https://phabricator.services.mozilla.com/D198790

--- style/properties/data.py
+++ style/properties/data.py
@@ -881,12 +881,14 @@ def _remove_common_first_line_and_first_letter_properties(props, engine):
         props.remove("text-emphasis-position")
         props.remove("text-emphasis-style")
         props.remove("text-emphasis-color")
+        props.remove("text-wrap-mode")
+        props.remove("white-space-collapse")
+    else:
+        props.remove("white-space")
 
     props.remove("overflow-wrap")
     props.remove("text-align")
     props.remove("text-justify")
-    props.remove("white-space-collapse")
-    props.remove("text-wrap-mode")
     props.remove("text-wrap-style")
     props.remove("word-break")
     props.remove("text-indent")

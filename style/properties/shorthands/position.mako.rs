9963d8808bcbb6e990592a06d79c01aa968750c2
Nico Burns <nico@nicoburns.com>
2024-06-20 13:57:31 +1200

    Enable grid styles for servo

--- style/properties/shorthands/position.mako.rs
+++ style/properties/shorthands/position.mako.rs
@@ -164,7 +164,8 @@
 <%helpers:shorthand
     name="grid-${kind}"
     sub_properties="grid-${kind}-start grid-${kind}-end"
-    engines="gecko",
+    engines="gecko servo",
+    servo_pref="layout.grid.enabled",
     spec="https://drafts.csswg.org/css-grid/#propdef-grid-${kind}"
 >
     use crate::values::specified::GridLine;
@@ -218,7 +219,8 @@
 
 <%helpers:shorthand
     name="grid-area"
-    engines="gecko"
+    engines="gecko servo"
+    servo_pref="layout.grid.enabled",
     sub_properties="grid-row-start grid-row-end grid-column-start grid-column-end"
     spec="https://drafts.csswg.org/css-grid/#propdef-grid-area"
 >
@@ -313,7 +315,8 @@
 
 <%helpers:shorthand
     name="grid-template"
-    engines="gecko"
+    engines="gecko servo"
+    servo_pref="layout.grid.enabled",
     sub_properties="grid-template-rows grid-template-columns grid-template-areas"
     spec="https://drafts.csswg.org/css-grid/#propdef-grid-template"
 >
@@ -554,7 +557,8 @@
 
 <%helpers:shorthand
     name="grid"
-    engines="gecko"
+    engines="gecko servo"
+    servo_pref="layout.grid.enabled",
     sub_properties="grid-template-rows grid-template-columns grid-template-areas
                     grid-auto-rows grid-auto-columns grid-auto-flow"
     spec="https://drafts.csswg.org/css-grid/#propdef-grid"

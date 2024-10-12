ea5420e19ebab325290e37c87c9d11e355ba4532
Nico Burns <nico@nicoburns.com>
2024-10-12 15:46:13 +0100

    Update metadata for stylo_traits crate

--- style_traits/lib.rs
+++ style_traits/lib.rs
@@ -6,7 +6,7 @@
 //! The traits are here instead of in script so that the devtools crate can be
 //! modified independently of the rest of Servo.
 
-#![crate_name = "style_traits"]
+#![crate_name = "stylo_traits"]
 #![crate_type = "rlib"]
 #![deny(unsafe_code, missing_docs)]
 

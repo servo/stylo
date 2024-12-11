098f1bef3e231bbf86038c0a2b1ddc31031dc422
arthmis <artmis9@protonmail.com>
2024-12-11 15:04:52 -0500

    Add getter for prefers color scheme value found on Device (#100)
    * add getter for prefers color scheme value found on Device
    
    * update doc comment
    
    Co-authored-by: Martin Robinson <mrobinson@igalia.com>
    Signed-off-by: arthmis <artmis9@protonmail.com>
    
    ---------
    
    Signed-off-by: arthmis <artmis9@protonmail.com>
    Co-authored-by: lazypassion <25536767+lazypassion@users.noreply.github.com>
    Co-authored-by: Martin Robinson <mrobinson@igalia.com>

--- style/servo/media_queries.rs
+++ style/servo/media_queries.rs
@@ -301,6 +301,11 @@ impl Device {
         AbsoluteColor::BLACK
     }
 
+    /// Returns the color scheme of this [`Device`].
+    pub fn color_scheme(&self) -> PrefersColorScheme {
+        self.prefers_color_scheme
+    }
+
     pub(crate) fn is_dark_color_scheme(&self, _: ColorSchemeFlags) -> bool {
         false
     }

f288bcde081485b957e62a819b38b6031348fc0e
Nico Burns <nico@nicoburns.com>
2025-07-04 14:28:42 +0100

    Convert data.py to Rust WIP

--- stylo_codegen/src/data/mod.rs
+++ stylo_codegen/src/data/mod.rs
@@ -2,4 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-struct Foo;
+mod counted_unknown_properties;
+mod data;
+mod keyword;
+mod property;
+
+pub use data::*;
+pub use keyword::Keyword;
+pub use property::{EnabledIn, Property, StyleRuleTypes};
--- stylo_codegen/src/lib.rs
+++ stylo_codegen/src/lib.rs
@@ -3,3 +3,16 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 mod data;
+mod util;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum Engine {
+    Gecko,
+    Servo,
+}
+
+impl Engine {
+    pub fn is_gecko(self) -> bool {
+        matches!(self, Self::Gecko)
+    }
+}

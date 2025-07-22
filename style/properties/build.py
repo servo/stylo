728141d23a809af14cb83dee5166c66ae87166be
Nico Burns <nico@nicoburns.com>
2025-07-22 12:53:28 +0100

    Vendor markupsafe library
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- style/properties/build.py
+++ style/properties/build.py
@@ -8,7 +8,8 @@ import re
 import sys
 
 BASE = os.path.dirname(__file__.replace("\\", "/"))
-sys.path.insert(0, os.path.join(BASE, "mako-1.3.10-py3-none-any.whl"))
+sys.path.insert(0, os.path.join(BASE, "vendored_python", "mako-1.3.10-py3-none-any.whl"))
+sys.path.insert(0, os.path.join(BASE, "vendored_python")) # For importing markupsafe
 sys.path.insert(0, BASE)  # For importing `data.py`
 
 from mako import exceptions

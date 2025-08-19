63f6cd46b12b81b1083bb50fc845cd7601fd14cb
Nico Burns <nico@nicoburns.com>
2025-08-19 23:35:34 +0000

    Bug 1983603: Add device_pixel_ratio_ignoring_full_zoom method to Servo Device (r=firefox-style-system-reviewers,emilio)
    Gecko's Stylo Device already had this helper method.
    
    Signed-off-by: Nico Burns <nico@nicoburns.com>
    
    Differential Revision: https://phabricator.services.mozilla.com/D261580

--- style/servo/media_queries.rs
+++ style/servo/media_queries.rs
@@ -256,6 +256,11 @@ impl Device {
         (AU_PER_PX as f32 / self.device_pixel_ratio.0) as i32
     }
 
+    /// Returns the device pixel ratio, ignoring the full zoom factor.
+    pub fn device_pixel_ratio_ignoring_full_zoom(&self) -> Scale<f32, CSSPixel, DevicePixel> {
+        self.device_pixel_ratio
+    }
+
     /// Returns the device pixel ratio.
     pub fn device_pixel_ratio(&self) -> Scale<f32, CSSPixel, DevicePixel> {
         self.device_pixel_ratio

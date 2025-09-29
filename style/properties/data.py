0c97c0d24373e2ca4f2c0063f6b9a06e107605dd
Oriol Brufau <obrufau@igalia.com>
2025-09-29 13:30:52 +0000

    Bug 1991292 - Allow text-shadow property on ::marker. r=firefox-style-system-reviewers,emilio
    This property applies to text, so authors should be able to set it on
    ::marker so that it inherits to and takes effect on its text contents.
    See https://drafts.csswg.org/css-lists-3/#marker-properties
    
    Differential Revision: https://phabricator.services.mozilla.com/D266527

--- style/properties/data.py
+++ style/properties/data.py
@@ -1077,15 +1077,16 @@ class PropertyRestrictions:
         return set(
             [
                 "color",
+                "content",
+                "direction",
+                "line-height",
                 "text-combine-upright",
                 "text-emphasis-color",
                 "text-emphasis-position",
                 "text-emphasis-style",
+                "text-shadow",
                 "text-transform",
                 "unicode-bidi",
-                "direction",
-                "content",
-                "line-height",
                 "-moz-osx-font-smoothing",
             ]
             + PropertyRestrictions.shorthand(data, "text-wrap")

706d1a47410a5a134ca8a5b3f2969fd0501e3dc5
David Shin <dshin@mozilla.com>
2025-10-23 16:34:14 +0000

    Bug 1996051: Let the ceiling of the outer scope be a candidate for the inner scope. r=firefox-style-system-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D269785

--- style/stylesheets/scope_rule.rs
+++ style/stylesheets/scope_rule.rs
@@ -302,9 +302,6 @@ where
     let mut parent = Some(element);
     let mut proximity = 0usize;
     while let Some(p) = parent {
-        if ceiling == Some(p.opaque()) {
-            break;
-        }
         if target.check(p, ceiling, scope_subject_map, context) {
             result.push(ScopeRootCandidate {
                 root: p.opaque(),
@@ -312,6 +309,9 @@ where
             });
             // Note that we can't really break here - we need to consider
             // ALL scope roots to figure out whch one didn't end.
+        }
+        if ceiling == Some(p.opaque()) {
+            break;
         }
         parent = p.parent_element();
         proximity += 1;

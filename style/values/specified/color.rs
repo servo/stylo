2048215b46802022f0f7952ecb126f460bcad815
Tiaan Louw <tlouw@mozilla.com>
2025-11-25 12:03:18 +0000

    Bug 2002148 - Handle oklab as the default interpolation method for color-mix r=layout-reviewers,firefox-style-system-reviewers,emilio
    The oklab color space is now handled as default, so color-mix now
    handles parsing and serialization accordingly.
    
    See: https://github.com/web-platform-tests/interop/issues/1166
    
    Differential Revision: https://phabricator.services.mozilla.com/D273982

--- style/values/specified/color.rs
+++ style/values/specified/color.rs
@@ -33,8 +33,15 @@ impl ColorMix {
         input.expect_function_matching("color-mix")?;
 
         input.parse_nested_block(|input| {
-            let interpolation = ColorInterpolationMethod::parse(context, input)?;
-            input.expect_comma()?;
+            // If the color interpolation method is omitted, default to "in oklab".
+            // See: https://github.com/web-platform-tests/interop/issues/1166
+            let interpolation = input
+                .try_parse(|input| -> Result<_, ParseError<'i>> {
+                    let interpolation = ColorInterpolationMethod::parse(context, input)?;
+                    input.expect_comma()?;
+                    Ok(interpolation)
+                })
+                .unwrap_or_default();
 
             let try_parse_percentage = |input: &mut Parser| -> Option<Percentage> {
                 input

c90d1a80d114f23fe5fc56a80dfa08735087ea8d
Vincent Hilla <vhilla@mozilla.com>
2025-12-16 18:12:05 +0000

    Bug 1858562 - Part 1: Implement Document Picture-in-Picture Spec. r=edgar,smaug,dom-core,webidl,firefox-style-system-reviewers,layout-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D265290

--- style/gecko/media_features.rs
+++ style/gecko/media_features.rs
@@ -8,6 +8,7 @@ use crate::derives::*;
 use crate::gecko_bindings::bindings;
 use crate::gecko_bindings::structs;
 use crate::media_queries::{Device, MediaType};
+use crate::parser::ParserContext;
 use crate::queries::feature::{AllowsRanges, Evaluator, FeatureFlags, QueryFeatureDescription};
 use crate::queries::values::{Orientation, PrefersColorScheme};
 use crate::values::computed::{CSSPixelLength, Context, Ratio, Resolution};
@@ -77,6 +78,10 @@ fn eval_device_orientation(context: &Context, value: Option<Orientation>) -> boo
     Orientation::eval(device_size(context.device()), value)
 }
 
+fn document_picture_in_picture_enabled(_: &ParserContext) -> bool {
+    static_prefs::pref!("dom.documentpip.enabled")
+}
+
 /// Values for the display-mode media feature.
 #[derive(Clone, Copy, Debug, FromPrimitive, Parse, PartialEq, ToCss)]
 #[repr(u8)]
@@ -86,6 +91,8 @@ pub enum DisplayMode {
     MinimalUi,
     Standalone,
     Fullscreen,
+    #[parse(condition = "document_picture_in_picture_enabled")]
+    PictureInPicture,
 }
 
 /// https://w3c.github.io/manifest/#the-display-mode-media-feature

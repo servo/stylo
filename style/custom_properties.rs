1793edab2439d101633d4d43331ec37abbe3cd48
Emilio Cobos Álvarez <emilio@crisal.io>
2026-02-02 22:16:38 +0100

    Bug 2013337 - Fix a regression with trailing unclosed comments.

--- style/custom_properties.rs
+++ style/custom_properties.rs
@@ -891,6 +891,7 @@ fn parse_declaration_value_block<'i, 't>(
                                 first_token_type: first,
                                 last_token_type: last,
                             });
+                            input_end_position = Some(input.position());
                         } else {
                             let state = input.state();
                             // We still need to consume the rest of the potentially-unclosed
@@ -902,9 +903,9 @@ fn parse_declaration_value_block<'i, 't>(
                                 references,
                                 missing_closing_characters,
                             )?;
+                            input_end_position = Some(input.position());
                             input.reset(&state);
                         }
-                        input_end_position = Some(input.position());
                         Ok(fallback)
                     })?;
                     if input_end_position.unwrap() == input.position() {

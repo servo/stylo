3b46f3ef27eb14ca276b4bf3ff041a6c43d93827
Oriol Brufau <obrufau@igalia.com>
2026-02-04 15:41:33 +0100

    Sort static prefs by name (#303)
    Signed-off-by: Oriol Brufau <obrufau@igalia.com>

--- stylo_static_prefs/src/lib.rs
+++ stylo_static_prefs/src/lib.rs
@@ -6,37 +6,10 @@
 //! be kept sync with the preferences used by the style.
 #[macro_export]
 macro_rules! pref {
-    ("layout.css.stylo-local-work-queue.in-main-thread") => {
-        32
-    };
-    ("layout.css.stylo-work-unit-size") => {
-        16
-    };
-    ("layout.css.stylo-local-work-queue.in-worker") => {
-        0
-    };
-    ("layout.css.system-ui.enabled") => {
+    ("layout.css.contrast-color.enabled") => {
         true
     };
     ("layout.css.fit-content-function.enabled") => {
-        true
-    };
-    ("layout.css.outline-offset.snapping") => {
-        1
-    };
-    ("layout.css.relative-color-syntax.enabled") => {
-        true
-    };
-    ("layout.css.stretch-size-keyword.enabled") => {
-        true
-    };
-    ("layout.css.marker.restricted") => {
-        true
-    };
-    ("layout.css.webkit-fill-available.enabled") => {
-        true
-    };
-    ("layout.css.webkit-fill-available.all-size-properties.enabled") => {
         true
     };
     ("layout.css.font-variations.enabled") => {
@@ -45,10 +18,37 @@ macro_rules! pref {
     ("layout.css.gradient-color-interpolation-method.enabled") => {
         true
     };
-    ("layout.css.contrast-color.enabled") => {
+    ("layout.css.marker.restricted") => {
         true
     };
+    ("layout.css.outline-offset.snapping") => {
+        1
+    };
     ("layout.css.properties-and-values.enabled") => {
+        true
+    };
+    ("layout.css.relative-color-syntax.enabled") => {
+        true
+    };
+    ("layout.css.stretch-size-keyword.enabled") => {
+        true
+    };
+    ("layout.css.stylo-local-work-queue.in-main-thread") => {
+        32
+    };
+    ("layout.css.stylo-local-work-queue.in-worker") => {
+        0
+    };
+    ("layout.css.stylo-work-unit-size") => {
+        16
+    };
+    ("layout.css.system-ui.enabled") => {
+        true
+    };
+    ("layout.css.webkit-fill-available.all-size-properties.enabled") => {
+        true
+    };
+    ("layout.css.webkit-fill-available.enabled") => {
         true
     };
     ($string:literal) => {

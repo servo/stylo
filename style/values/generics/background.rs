bac8174ea7213d6e6875fa3c595de5562ea1dc88
Jan Varga <jvarga@igalia.com>
2026-03-21 11:03:44 +0000

    Bug 2017250 - Demonstrate ToTyped derive on multi-field BackgroundSize type; r=emilio,firefox-style-system-reviewers
    Demonstrate multi-field support in the ToTyped derive using the
    background-size property.
    
    The ExplicitSize variant contains separate width and height fields.
    Reification now derives TypedValue items for both fields, using
    `#[typed_value(skip_if = "...")]` to omit the height when it is `auto`.
    
    This allows cases such as `background-size: 3.14px` or `200px 100px` to
    reify correctly (the former producing a CSSUnitValue while the latter falls
    back to CSSUnsupportedValue).
    
    Differential Revision: https://phabricator.services.mozilla.com/D286663

--- style/values/generics/background.rs
+++ style/values/generics/background.rs
@@ -26,6 +26,7 @@ use crate::values::generics::length::{GenericLengthPercentageOrAuto, LengthPerce
     ToTyped,
 )]
 #[repr(C, u8)]
+#[typed_value(derive_fields)]
 pub enum GenericBackgroundSize<LengthPercent> {
     /// `<width> <height>`
     ExplicitSize {
@@ -33,6 +34,7 @@ pub enum GenericBackgroundSize<LengthPercent> {
         width: GenericLengthPercentageOrAuto<LengthPercent>,
         /// Explicit height.
         #[css(contextual_skip_if = "width_and_height_are_auto")]
+        #[typed_value(skip_if = "GenericLengthPercentageOrAuto::is_auto")]
         height: GenericLengthPercentageOrAuto<LengthPercent>,
     },
     /// `cover`

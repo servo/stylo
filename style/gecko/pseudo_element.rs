f52471226fc29796d3b620a1e6f5ab994de7b3e7
narjoDev <63086420+narjoDev@users.noreply.github.com>
2026-03-26 10:42:10 +0000

    Bug 1899687 - Remove layout.css.marker.restricted pref. r=emilio,devtools-reviewers,firefox-style-system-reviewers,layout-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D289002

--- style/gecko/pseudo_element.rs
+++ style/gecko/pseudo_element.rs
@@ -497,9 +497,7 @@ impl PseudoElement {
             PseudoElement::FirstLine => PropertyFlags::APPLIES_TO_FIRST_LINE,
             PseudoElement::Placeholder => PropertyFlags::APPLIES_TO_PLACEHOLDER,
             PseudoElement::Cue => PropertyFlags::APPLIES_TO_CUE,
-            PseudoElement::Marker if static_prefs::pref!("layout.css.marker.restricted") => {
-                PropertyFlags::APPLIES_TO_MARKER
-            },
+            PseudoElement::Marker => PropertyFlags::APPLIES_TO_MARKER,
             _ => return None,
         })
     }

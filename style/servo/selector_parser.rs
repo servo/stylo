204f4eb4fb2a8fc81f733c2b642008b2e32d07a2
Kelechi Ebiri <ebiritg@gmail.com>
2026-04-01 23:11:04 +0100

    clean-up: remove PseudoElement::DetailsSummary from stylo
    Signed-off-by: Kelechi Ebiri <ebiritg@gmail.com>

--- style/servo/selector_parser.rs
+++ style/servo/selector_parser.rs
@@ -98,7 +98,6 @@ impl ToCss for PseudoElement {
             Selection => "::selection",
             FirstLetter => "::first-letter",
             Backdrop => "::backdrop",
-            DetailsSummary => "::-servo-details-summary",
             DetailsContent => "::details-content",
             Marker => "::marker",
             ColorSwatch => "::color-swatch",
@@ -256,7 +255,6 @@ impl PseudoElement {
             PseudoElement::Backdrop
             | PseudoElement::ColorSwatch
             | PseudoElement::FileSelectorButton
-            | PseudoElement::DetailsSummary
             | PseudoElement::Marker
             | PseudoElement::Placeholder
             | PseudoElement::DetailsContent
@@ -679,12 +677,6 @@ impl<'a, 'i> ::selectors::Parser<'i> for SelectorParser<'a> {
             "file-selector-button" => FileSelectorButton,
             "first-letter" => FirstLetter,
             "marker" => Marker,
-            "-servo-details-summary" => {
-                if !self.in_user_agent_stylesheet() {
-                    return Err(location.new_custom_error(SelectorParseErrorKind::UnexpectedIdent(name.clone())))
-                }
-                DetailsSummary
-            },
             "details-content" => DetailsContent,
             "color-swatch" => ColorSwatch,
             "placeholder" => Placeholder,

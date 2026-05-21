4218240105c64db13d00f263074af7a376a4212e
Keith Cirkel <keithamus@users.noreply.github.com>
2026-05-21 22:24:30 +0000

    Bug 1974383 - Implement headingoffset r=dom-core,accessibility-platform-reviewers,webidl,firefox-style-system-reviewers,smaug,emilio,Jamie,saschanaz
    Differential Revision: https://phabricator.services.mozilla.com/D255837

--- style/gecko/selector_parser.rs
+++ style/gecko/selector_parser.rs
@@ -225,7 +225,7 @@ impl NonTSPseudoClass {
             return static_prefs::pref!("dom.viewTransitions.enabled");
         }
         if matches!(*self, Self::Heading(..)) {
-            return static_prefs::pref!("layout.css.heading-selector.enabled");
+            return static_prefs::pref!("dom.headingoffset.enabled");
         }
         if matches!(
             *self,

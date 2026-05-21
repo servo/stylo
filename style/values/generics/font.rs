2a26af3c3debec96117f051a73b8f124c041fbc3
Simon Wülker <simon.wuelker@arcor.de>
2026-05-21 13:37:41 +0200

    Enable "font-feature-settings" for servo
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- style/values/generics/font.rs
+++ style/values/generics/font.rs
@@ -37,6 +37,7 @@ pub trait TaggedFontValue {
     ToResolvedValue,
     ToShmem,
 )]
+#[cfg_attr(feature = "servo", derive(Deserialize, Hash, Serialize))]
 pub struct FeatureTagValue<Integer> {
     /// A four-character tag, packed into a u32 (one byte per character).
     pub tag: FontTag,
@@ -116,7 +117,7 @@ impl<T> TaggedFontValue for VariationValue<T> {
     ToShmem,
     ToTyped,
 )]
-#[cfg_attr(feature = "servo", derive(Deserialize, Serialize))]
+#[cfg_attr(feature = "servo", derive(Deserialize, Hash, Serialize))]
 #[css(comma)]
 #[typed(todo_derive_fields)]
 pub struct FontSettings<T>(#[css(if_empty = "normal", iterable)] pub Box<[T]>);
@@ -170,7 +171,7 @@ impl<T: Parse> Parse for FontSettings<T> {
     ToResolvedValue,
     ToShmem,
 )]
-#[cfg_attr(feature = "servo", derive(Deserialize, Serialize))]
+#[cfg_attr(feature = "servo", derive(Deserialize, Hash, Serialize))]
 pub struct FontTag(pub u32);
 
 impl ToCss for FontTag {

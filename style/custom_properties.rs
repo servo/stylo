b233a95c06663d742b2a542bac608511ce3edb27
rperta <rperta@mozilla.com>
2026-05-26 23:04:56 +0300

    Revert "Bug 2006626 - Fix overpermissive parsing of <syntax-single-component>. r=emilio,firefox-style-system-reviewers" for causing wpt failures at attr-all-types.html
    This reverts commit e4958f50728de6206ebba89f988dbc67f9844df5.
    
    Revert "Bug 2006626 - Take fallback in attr() when <attr-type> is invalid. r=emilio,firefox-style-system-reviewers"
    
    This reverts commit a106c9a24f37a91ac8cbad0ed7676942d0bee8cf.

--- style/custom_properties.rs
+++ style/custom_properties.rs
@@ -652,7 +652,6 @@ impl ComputedSubstitutionFunctions {
 
 #[derive(Clone, Debug, MallocSizeOf, PartialEq, ToShmem, Parse)]
 enum AttributeType {
-    Invalid,
     None,
     RawString,
     Type(SyntaxDescriptor),
@@ -1269,10 +1268,11 @@ fn parse_attr_type<'i, 't>(input: &mut Parser<'i, 't>) -> AttributeType {
                 Token::Ident(ref ident) => {
                     if ident.eq_ignore_ascii_case("raw-string") {
                         AttributeType::RawString
-                    } else if let Ok(unit) = AttrUnit::from_ident(ident) {
-                        AttributeType::Unit(unit)
                     } else {
-                        AttributeType::Invalid
+                        let unit = AttrUnit::from_ident(ident).map_err(|_| {
+                            input.new_custom_error(StyleParseErrorKind::UnspecifiedError)
+                        })?;
+                        AttributeType::Unit(unit)
                     }
                 },
                 Token::Delim('%') => AttributeType::Unit(AttrUnit::Percentage),
@@ -2832,7 +2832,6 @@ fn substitute_one_reference<'a>(
                             AttributeType::RawString | AttributeType::None => {
                                 simple_attr_subst(&attr)
                             },
-                            AttributeType::Invalid => None,
                         }
                     },
                 )

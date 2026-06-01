a81b1142623fa096bb78b17c4e4c3bea8def54c3
Jonathan Kew <jkew@mozilla.com>
2026-06-01 14:49:14 +0000

    Bug 2044033 - Check for changes to properties that could affect line height. r=diegociudadreale,firefox-style-system-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D303779

--- style/values/generics/font.rs
+++ style/values/generics/font.rs
@@ -354,4 +354,10 @@ impl<N, L> LineHeight<N, L> {
     pub fn normal() -> Self {
         LineHeight::Normal
     }
+
+    /// Returns whether the value is `normal`.
+    #[inline]
+    pub fn is_normal(&self) -> bool {
+        matches!(self, Self::Normal)
+    }
 }

e44761e93542a2042c7beecb0f428d540c49654a
Mohamed Mostafa <mu-mostafa98@gmail.com>
2026-06-06 16:52:00 +0800

    Enable SVG CSS properties for Servo
    - Remove engine = gecko gating and servo_pref gating from SVG
    properties and shorthands.
    - Update size_of_test.
    
    Signed-off-by: Mohamed Mostafa mu-mostafa98@gmail.com

--- style/properties/properties.mako.rs
+++ style/properties/properties.mako.rs
@@ -2838,7 +2838,7 @@ macro_rules! longhand_properties_idents {
 #[cfg(feature = "gecko")]
 size_of_test!(ComputedValues, 248);
 #[cfg(feature = "servo")]
-size_of_test!(ComputedValues, 224);
+size_of_test!(ComputedValues, 232);
 
 // FFI relies on this.
 size_of_test!(Option<Arc<ComputedValues>>, 8);

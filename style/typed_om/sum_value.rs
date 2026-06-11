7b70b69dd50ca012445a5c9bb8e8d84d21fefe44
Jan Varga <jvarga@igalia.com>
2026-06-11 18:20:01 +0000

    Bug 2044967 - Add CSSMathInvert support to SumValue creation; r=firefox-style-system-reviewers,dshin
    Extend SumValue creation from NumericValue to support CSSMathInvert values.
    
    Implement the "create a sum value" algorithm for CSSMathInvert, including
    inversion of the underlying sum value items.
    
    This expands the set of CSSNumericValue objects that can be represented as
    sum values and is another step towards completing CSSNumericValue.to().
    
    Differential Revision: https://phabricator.services.mozilla.com/D304754

--- style/typed_om/sum_value.rs
+++ style/typed_om/sum_value.rs
@@ -195,9 +195,25 @@ impl SumValue {
             },
 
             // CSSMathInvert
-            NumericValue::Math(MathValue::Invert(_math_invert)) => {
-                // TODO: Implement me!
-                Err(())
+            NumericValue::Math(MathValue::Invert(math_invert)) => {
+                // Step 1 & 2.
+                let mut values = SumValue::try_from_numeric_value(math_invert)?.0;
+
+                // Step 3.
+                if values.len() != 1 {
+                    return Err(());
+                }
+
+                let item = &mut values[0];
+
+                // Step 4.
+                item.value = 1.0 / item.value;
+                for power in item.unit_map.values_mut() {
+                    *power = -*power;
+                }
+
+                // Step 5.
+                Ok(Self(values))
             },
 
             // CSSMathMin

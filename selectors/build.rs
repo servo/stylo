bc066288cd32b7285dc1a29c6bc8368b05f23c2a
Nico Burns <nico@nicoburns.com>
2026-06-14 20:00:30 +0000

    Bug 2042048: Make selectors's dependency on phf optional (r=firefox-style-system-reviewers,emilio)
    Signed-off-by: Nico Burns <nico@nicoburns.com>
    
    Differential Revision: https://phabricator.services.mozilla.com/D302243

--- selectors/build.rs
+++ selectors/build.rs
@@ -2,28 +2,38 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-extern crate phf_codegen;
-
-use std::env;
-use std::fs::File;
-use std::io::{BufWriter, Write};
-use std::path::Path;
-
 fn main() {
+    use std::env;
+    use std::path::Path;
+
     let path = Path::new(&env::var_os("OUT_DIR").unwrap())
         .join("ascii_case_insensitive_html_attributes.rs");
-    let mut file = BufWriter::new(File::create(&path).unwrap());
 
-    let mut set = phf_codegen::Set::new();
-    for name in ASCII_CASE_INSENSITIVE_HTML_ATTRIBUTES.split_whitespace() {
-        set.entry(name);
-    }
-    write!(
-        &mut file,
-        "{{ static SET: ::phf::Set<&'static str> = {}; &SET }}",
-        set.build(),
-    )
-    .unwrap();
+    #[cfg(feature = "fast_attr_match")]
+    let file_content: String = {
+        let mut set = phf_codegen::Set::new();
+        for name in ASCII_CASE_INSENSITIVE_HTML_ATTRIBUTES.split_whitespace() {
+            set.entry(name);
+        }
+        format!(
+            "{{ static SET: ::phf::Set<&'static str> = {}; &SET }}",
+            set.build(),
+        )
+    };
+
+    #[cfg(not(feature = "fast_attr_match"))]
+    let file_content: String = {
+        use std::fmt::Write;
+
+        let mut out = "[".to_string();
+        for name in ASCII_CASE_INSENSITIVE_HTML_ATTRIBUTES.split_whitespace() {
+            write!(&mut out, "\"{name}\",").unwrap();
+        }
+        out.write_char(']').unwrap();
+        out
+    };
+
+    std::fs::write(&path, file_content).unwrap();
 }
 
 /// <https://html.spec.whatwg.org/multipage/#selectors>

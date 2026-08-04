8f5a10985c2b237ff0ebe6424138dc2e4ae66ea8
Simon Sapin <simon@igalia.com>
2026-08-04 13:17:54 +0200

    Fix unit tests and run them on CI
    Signed-off-by: Simon Sapin <simon@igalia.com>

--- style/servo/shadow_parts.rs
+++ style/servo/shadow_parts.rs
@@ -192,7 +192,7 @@ mod tests {
 
     #[test]
     fn parse_valid_mapping_list() {
-        let mut mappings = parse_mapping_list("foo: bar, totally-invalid-mapping,,");
+        let mut mappings = parse_mapping_list("foo: bar, totally-invalid-mapping:,,");
 
         // "foo: bar" is a valid mapping
         assert_eq!(
@@ -202,6 +202,6 @@ mod tests {
         );
         // "totally-invalid-mapping" is not a valid mapping and should be ignored
         // "" is not valid (and consists of nothing but whitespace), so it should be ignored
-        assert!(mappings.next().is_none(), "No more mappings should exist");
+        assert_eq!(mappings.next(), None, "No more mappings should exist");
     }
 }

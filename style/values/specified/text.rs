a35205314429cdf0c3950de49a80372d0510b8b8
Simon Sapin <simon@igalia.com>
2026-09-02 21:36:16 +0000

    Bug 2068247 - Enable `text-transform: math-auto` for Servo. r=firefox-style-system-reviewers,emilio
    This imports https://github.com/servo/stylo/pull/442
    
    However, it has been modified to keep `math-auto` as a case transform.
    That's because Gecko expects to get it when doing a bitwise AND with
    StyleTextTransform::CASE_TRANSFORMS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D322781

--- style/values/specified/text.rs
+++ style/values/specified/text.rs
@@ -361,7 +361,6 @@ pub enum TextTransformCase {
     /// Capitalize each word.
     Capitalize,
     /// Automatic italicization of math variables.
-    #[cfg(feature = "gecko")]
     MathAuto,
 }
 
@@ -381,22 +380,11 @@ pub enum TextTransformCase {
     ToShmem,
     ToTyped,
 )]
-#[cfg_attr(
-    feature = "gecko",
-    css(bitflags(
-        single = "none,math-auto",
-        mixed = "uppercase,lowercase,capitalize,full-width,full-size-kana",
-        validate_mixed = "Self::validate_mixed_flags",
-    ))
-)]
-#[cfg_attr(
-    not(feature = "gecko"),
-    css(bitflags(
-        single = "none",
-        mixed = "uppercase,lowercase,capitalize,full-width,full-size-kana",
-        validate_mixed = "Self::validate_mixed_flags",
-    ))
-)]
+#[css(bitflags(
+    single = "none,math-auto",
+    mixed = "uppercase,lowercase,capitalize,full-width,full-size-kana",
+    validate_mixed = "Self::validate_mixed_flags",
+))]
 #[repr(C)]
 /// Specified value for the text-transform property.
 /// (The spec grammar gives
@@ -414,15 +402,11 @@ bitflags! {
         /// Capitalize each word.
         const CAPITALIZE = 1 << 2;
         /// Automatic italicization of math variables.
-        #[cfg(feature = "gecko")]
         const MATH_AUTO = 1 << 3;
 
         /// All the case transforms, which are exclusive with each other.
-        #[cfg(feature = "gecko")]
+        /// Except for math-auto, they can be mixed with full-width or full-size-kana.
         const CASE_TRANSFORMS = Self::UPPERCASE.0 | Self::LOWERCASE.0 | Self::CAPITALIZE.0 | Self::MATH_AUTO.0;
-        /// All the case transforms, which are exclusive with each other.
-        #[cfg(feature = "servo")]
-        const CASE_TRANSFORMS = Self::UPPERCASE.0 | Self::LOWERCASE.0 | Self::CAPITALIZE.0;
 
         /// full-width
         const FULL_WIDTH = 1 << 4;
@@ -457,7 +441,6 @@ impl TextTransform {
             Self::UPPERCASE => TextTransformCase::Uppercase,
             Self::LOWERCASE => TextTransformCase::Lowercase,
             Self::CAPITALIZE => TextTransformCase::Capitalize,
-            #[cfg(feature = "gecko")]
             Self::MATH_AUTO => TextTransformCase::MathAuto,
             _ => unreachable!("Case bits are exclusive with each other"),
         }
